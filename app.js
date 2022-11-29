299963844cbe6c1e95852bb5ace08c8dbb1a2ea6
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-11-28 22:18:12 -0800

    confugure session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override');
 
@@ -24,6 +26,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); //parse body when adding new campground
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'badsecret', // change in production
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true, // security measure (default is true)
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // a week from now
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
