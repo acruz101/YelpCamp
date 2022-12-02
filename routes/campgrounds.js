081b0ea9f63c34556a758bd918730b685c580999
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-12-02 14:46:38 -0800

    reviews authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,13 +22,19 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    const campground = await Campground.findById(req.params.id).populate({
+        path:'reviews', 
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     console.log(campground);
     if(!campground) {
         req.flash('error', 'No campground found!');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/show', { campground });
+    
 }));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
