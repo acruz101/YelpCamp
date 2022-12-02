ed62d7d0c9df845102ebcd735767265fe55d2b99
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-12-01 22:05:42 -0800

    login routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -19,5 +20,16 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+// have passport authenticate with local strategy
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), async (req, res) => {
+    // if we make it this far with passport, user is authenticated
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds')
+})
 
 module.exports = router;
