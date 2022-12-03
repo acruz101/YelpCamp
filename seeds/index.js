77abe9c111552e9635b7b703b0da2385a1da33b7
acruz101 <47541514+acruz101@users.noreply.github.com>
2022-12-03 14:51:03 -0800

    IMG UPLOAD - fix seeds + add upload files to edit page

--- seeds/index.js
+++ seeds/index.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
 }
 
 const db = mongoose.connection;
@@ -23,13 +23,22 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     // const c = new Campground({title: 'Campground Fake'})
     // await c.save();
-    for(let i = 0; i < 50; i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://source.unsplash.com/collection/483251`,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dwrza7dr7/image/upload/v1670106137/YelpCamp/dgl29wsiyniecjksdseh.jpg',
+                    filename: 'YelpCamp/dgl29wsiyniecjksdseh'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dwrza7dr7/image/upload/v1670106138/YelpCamp/mhy2onw1xj5va0aelkjh.jpg',
+                    filename: 'YelpCamp/mhy2onw1xj5va0aelkjh'
+                }
+            ],
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla consequuntur sapiente quod maxime, optio repellat eum quam libero sint quasi architecto necessitatibus at illum magnam natus repellendus facilis facere quae?',
             price: randomPrice,
             author: '63896d0428a62996ac39353d'
